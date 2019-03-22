1f9fe9e9599b4151f14709dd275478da29e5140d
CClarkes <connorclarkes@gmail.com>
2019-03-22 00:39:26 +0000

    create user authentication capabilty

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController 
+
+def new
+end
+
+def create
+    user = User.new(user_params)
+    if user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+    else
+        redirect_to '/sessions/new'
+    end
+end
+
+def login
+    user = User.find_by_email(params[:email])
+    if user.password_digest == params[:password_digest]
+        give_token
+    else
+        redirect_to_home_url
+    end
+end
+
+private 
+
+def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
